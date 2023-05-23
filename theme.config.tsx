66caf6ea0a51531141376ad4511fc9ff867f00ff
bryanbill <emobrya@gmail.com>
2023-05-23 20:13:41 +0300

    updates links

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,39 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Zero is a fast, lightweight and scalable Dart backend framework"
+        />
+        <meta name="og:title" content="Zero" />
+        <meta
+          name="og:description"
+          content="Zero is a fast, lightweight and scalable Dart backend framework"
+        />
+      </head>
+    </>
+  ),
+  feedback: {
+    content: "Is this page helpful?",
+  },
+  editLink: {
+    text: "Edit this page on GitHub",
+  },
   logo: (
     <>
       <span className="font-extrabold text-2xl text-purple-500">Zero</span>
     </>
   ),
- 
+
   project: {
-    link: "https://github.com/bryanbill/zero",
+    link: "https://github.com/bryanbill/zero-docs",
   },
 
-  docsRepositoryBase: "https://github.com/bryanbill/zero",
+  docsRepositoryBase: "https://github.com/bryanbill/zero-docs/tree/main",
   footer: {
     text: "Zero",
   },
